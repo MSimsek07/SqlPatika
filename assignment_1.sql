d5e5cb20bba9b3d84b0f574cb7e928faccd11f59
Muhammet Ali Simsek <73036894+MSimsek07@users.noreply.github.com>
2022-07-17 10:48:07 +0300

    Update assignment_1.sql

--- assignment_1.sql
+++ assignment_1.sql
@@ -2,7 +2,7 @@
 SELECT title, description FROM film;
 
 --2)film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
-SELECT * FROM film WHERE length < 60 OR length > 75; 
+SELECT * FROM film WHERE length > 60 OR length < 75; 
 
 --3)film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 SELECT * FROM film WHERE rental_rate=0.99 AND (replacement_cost=12.99 OR replacement_cost = 28.99);
